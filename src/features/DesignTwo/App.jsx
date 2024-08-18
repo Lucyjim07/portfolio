55e7f1de642fca0603fc8d4851768b3ac9cfbc90
Lucy Jiménez <lucy.jimenez.mejia@gmail.com>
2024-08-17 22:42:54 -0500

    Crear primera sección de Hero para nuevo diseño, manejar theme para modos oscuro y claro agregar primer estilo con variables

---
+++ src/features/DesignTwo/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './sections/Hero/Hero'
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <span>
+        basado en el tutorial de https://www.youtube.com/watch?v=ZpIel9cv4Jk
+      </span>
+    </>
+   )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './features/DesignOne/App.jsx'
+// import App from './features/DesignOne/App.jsx'
+import App from './features/DesignTwo/App'
+import { ThemeProvider } from "./features/DesignTwo/common/ThemeContext";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
